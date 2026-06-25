84d7daccac550aa8120f261cae9ad49d41d9839b
Mshafiee1988 <shafiee.m1988@gmail.com>
2026-06-25 11:53:22 +0330

    Create test_app.py
    from src.app import app
    
    client = app.test_client()
    
    
    def test_get_students():
        # Arrange
    
        # Act
        response = client.get('/api/students')
    
        # Assert
        assert response.status_code == 200
    
    
    def test_get_existing_student():
        # Arrange
    
        # Act
        response = client.get('/api/students/1')
    
        # Assert
        assert response.status_code == 200
    
    
    def test_get_non_existing_student():
        # Arrange
    
        # Act
        response = client.get('/api/students/999')
    
        # Assert
        assert response.status_code == 404

---
+++ tests/test_app.py
@@ -0,0 +1,33 @@
+from src.app import app
+
+client = app.test_client()
+
+
+def test_get_students():
+    # Arrange
+
+    # Act
+    response = client.get('/api/students')
+
+    # Assert
+    assert response.status_code == 200
+
+
+def test_get_existing_student():
+    # Arrange
+
+    # Act
+    response = client.get('/api/students/1')
+
+    # Assert
+    assert response.status_code == 200
+
+
+def test_get_non_existing_student():
+    # Arrange
+
+    # Act
+    response = client.get('/api/students/999')
+
+    # Assert
+    assert response.status_code == 404
